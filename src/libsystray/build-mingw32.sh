39cea208df664940b1cc28ea13b38d5df45cff35
Benjamin Robin <benjarobin@gmail.com>
2013-09-22 20:54:23 +0200

    Update C_FLAGS for lib

--- src/libsystray/build-mingw32.sh
+++ src/libsystray/build-mingw32.sh
@@ -4,5 +4,5 @@ CC=i486-mingw32-
 
 export PKG_CONFIG_PATH=~/static-gtk2-mingw32-0.1-1/lib/pkgconfig
 rm -f libzimbrasystray.dll
-"${CC}gcc" --shared -static -O2 -o libzimbrasystray.dll zimbrasystray.c $(pkg-config --static --cflags --libs gtk+-2.0)
+"${CC}gcc" --shared -static -O2 -Wall -Wextra -Wunused -Wshadow -Wcast-qual -fvisibility=hidden -o libzimbrasystray.dll zimbrasystray.c $(pkg-config --static --cflags --libs gtk+-2.0)
 "${CC}strip" libzimbrasystray.dll
